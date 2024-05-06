d0d659eae605ee45b7bb850b1947f849bffac12a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-06 12:07:07 -0700

    revert to 872c473

--- core/core.go
+++ core/core.go
@@ -10,23 +10,16 @@ import (
 )
 
 func walkDir(dir string, maxFiles int, oldString, newString string, skipDirs []string, count *int) error {
-	dirRenames := make(map[string]string)
-
-	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+	return filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if d.IsDir() {
-			if containsIgnoreCase(skipDirs, d.Name()) {
-				return filepath.SkipDir
-			}
+		if d.IsDir() && containsIgnoreCase(skipDirs, d.Name()) {
+			return filepath.SkipDir
+		}
 
-			if path != dir && strings.Contains(d.Name(), oldString) {
-				newName := strings.ReplaceAll(d.Name(), oldString, newString)
-				dirRenames[path] = filepath.Join(filepath.Dir(path), newName)
-			}
-		} else {
+		if !d.IsDir() {
 			*count++
 			if *count > maxFiles {
 				return fmt.Errorf("exceeded maximum number of files (%d)", maxFiles)
@@ -35,21 +28,16 @@ func walkDir(dir string, maxFiles int, oldString, newString string, skipDirs []s
 			if err := processFile(path, oldString, newString); err != nil {
 				return err
 			}
+
+			if strings.Contains(d.Name(), oldString) {
+				if err := renameFile(path, oldString, newString); err != nil {
+					return err
+				}
+			}
 		}
 
 		return nil
 	})
-	if err != nil {
-		return err
-	}
-
-	for oldPath, newPath := range dirRenames {
-		if err := os.Rename(oldPath, newPath); err != nil {
-			return err
-		}
-	}
-
-	return nil
 }
 
 func containsIgnoreCase(slice []string, item string) bool {
@@ -74,19 +62,12 @@ func processFile(path, oldString, newString string) error {
 		return err
 	}
 
-	if strings.Contains(filepath.Base(path), oldString) {
-		if err := renameFile(path, oldString, newString); err != nil {
-			return err
-		}
-	}
-
 	return nil
 }
 
 func renameFile(path, oldString, newString string) error {
 	newName := strings.ReplaceAll(filepath.Base(path), oldString, newString)
 	newPath := filepath.Join(filepath.Dir(path), newName)
-
 	if err := os.Rename(path, newPath); err != nil {
 		return err
 	}
@@ -104,7 +85,6 @@ func Main(dir string, maxFiles int, replace string, skipDirs []string) {
 		fmt.Println("Error: --replace parameter should be in the format 'string1=string2'")
 		os.Exit(1)
 	}
-
 	oldString, newString := parts[0], parts[1]
 
 	if _, err := os.Stat(filepath.Join(dir, ".git")); errors.Is(err, os.ErrNotExist) {
